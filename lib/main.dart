e7cd8e7da7b64fe83382f50745b8af98889048e5
ZARIFMAC <uzbzarif@gmail.com>
2023-04-13 15:01:50 +0500

    user

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 Widget user() {
-  return Container();
+  return Container(
+    margin: EdgeInsets.all(10),
+    height: 100,
+    child: Row(children: [
+      CircleAvatar(
+        backgroundColor: Colors.blueAccent,
+      ),
+      Column(
+        children: [
+          Text('data'),
+          Text('data'),
+          Text('data'),
+        ],
+      )
+    ]),
+  );
 }
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Container(),
+      body: Container(
+        child: user(),
+      ),
     ),
   ));
 }
