1c0c0754da1b752e33f38a0d77efb7c5b5c43e3f
ZARIFMAC <uzbzarif@gmail.com>
2023-04-13 14:57:47 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget user() {
+  return Container();
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(),
+    ),
+  ));
+}
