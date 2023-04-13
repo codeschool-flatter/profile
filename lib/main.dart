c88df1c70fc22a8dd073cf25226e83599109ef3f
ZARIFMAC <uzbzarif@gmail.com>
2023-04-13 15:04:39 +0500

    center

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 Widget user() {
   return Container(
-    margin: EdgeInsets.all(10),
+    margin: EdgeInsets.all(20),
     height: 100,
     child: Row(children: [
       CircleAvatar(
+        radius: 50,
         backgroundColor: Colors.blueAccent,
       ),
       Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('data'),
           Text('data'),
